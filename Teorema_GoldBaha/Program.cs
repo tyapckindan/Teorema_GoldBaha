d10719b8ae2ac0f5495507b89d7b725b943e9efe
Данила Тяпкин <dtapkin6@gmail.com>
2024-04-05 10:45:18 +0300

    Добавьте файлы проекта.

---
+++ Teorema_GoldBaha/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int[] nums = new int[num];
+
+        for (int i = 1; i <= num; i++)
+            if (isSimpleNum(i) == true)
+                nums[i] = i;
+
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = 0; j < nums.Length; j++)
+                if (nums[i] + nums[j] == num)
+                    Console.WriteLine($"Пара простых чисел: {nums[i]}:{nums[j]}");
+    }
+    private static bool isSimpleNum(int num)
+    {
+        for (int i = 2; i <= num / 2; i++)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+}
